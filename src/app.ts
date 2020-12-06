3f1ff4f89270136784ee3dc8bd9549803dc76bf5
Dylan Hand <dylan.hand@gmail.com>
2020-12-06 21:15:31 +0100

    Add post method

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
+
+app.post('/test', (req, res) => {
+  const { test } = req.body;
+  res.send(`test: ${test}`);
+});
+
 app.listen(port, () => {
-//   if (err) {
-//     return console.error(err);
-//   }
-  return console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
