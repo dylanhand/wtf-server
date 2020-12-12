6a62211ee0f7cb37b7ab79336f4562a0d9b50a85
Dylan Hand <dylan.hand@gmail.com>
2020-12-12 21:19:54 +0100

    Add support for posting images, refactor

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,33 @@ import handlebars from 'handlebars';
 import fs from 'fs';
 import { exec } from 'child_process';
 
+type Image = {
+  filename: string;
+  base64: string;
+}
+
 type BlogPostBody = {
   title: string;
   date: string;
   body: string;
   tags: [string];
+  image: Image;
   commitMessage: string;
 }
 
-// This is the path where posts exists. It's where new posts from the web service will be put.
-const BLOG_PATH = '/Users/dylanhand/Projects/dylanhand.github.io/_posts';
+const PROJECT_PATH = '/Users/dylanhand/Projects/dylanhand.github.io';
 
-// This is the path from which the site deployment script should be run
-const DEPLOY_PATH = '/Users/dylanhand/Projects/dylanhand.github.io/';
+// This is the path where blog posts live.
+const BLOG_PATH = `${PROJECT_PATH}/_posts`;
+
+// This is where images will be put.
+const IMAGES_PATH = `${PROJECT_PATH}/assets/img`;
+
+// This is the path from which the site deployment script should be run.
+const DEPLOY_SCRIPT_PATH = PROJECT_PATH;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '8mb' }));
 const port = 3000;
 
 const renderMarkdown = (postBody: BlogPostBody): string => {
@@ -28,31 +39,41 @@ const renderMarkdown = (postBody: BlogPostBody): string => {
   return compiledTemplate(postBody);
 };
 
-const filename = (title: string, date: string) => {
+const postFilename = (title: string, date: string) => {
   const titleFormatted = title.toLowerCase().trim().replace(' ', '-');
   // TODO: validate dates. For now we assume the sender knows what they're doing
-  // dates must be in format 1969-04-20
+  // dates must be in format: 1969-04-20
   return `${date}-${titleFormatted}.md`;
 };
 
-app.post('/publish', (req, res) => {
-  const postBody: BlogPostBody = req.body;
-  // TODO:
-  // 1. create a markdown file with the post body and attributes
-  // 2. move the file to blog path  (get path from env file)
-  // 3. run interaction-less rake deploy
+const savePost = (postBody: BlogPostBody) => {
   const markdown = renderMarkdown(postBody);
-
-  const filePath = `${BLOG_PATH}/${filename(postBody.title, postBody.date)}`;
+  const filePath = `${BLOG_PATH}/${postFilename(postBody.title, postBody.date)}`;
   fs.writeFileSync(filePath, markdown);
+};
 
-  exec(`cd ${DEPLOY_PATH} ; rake 'deploy[${postBody.commitMessage}]'`, (err, stdout, stderr) => {
+const saveImage = (image: Image) => {
+  const imageBuffer = Buffer.from(image.base64, 'base64');
+  const filePath = `${IMAGES_PATH}/${image.filename}`;
+  fs.writeFileSync(filePath, imageBuffer);
+};
+
+const deploySite = (commitMessage: string) => {
+  exec(`cd ${DEPLOY_SCRIPT_PATH} ; rake 'deploy[${commitMessage}]'`, (err, stdout, stderr) => {
     console.log(err);
     console.log(stderr);
     console.log(stdout);
   });
+};
 
-  res.send(markdown);
+app.post('/publish', (req, res) => {
+  const postBody: BlogPostBody = req.body;
+
+  savePost(postBody);
+  saveImage(postBody.image);
+  deploySite(postBody.commitMessage);
+
+  res.send('success, baby');
 });
 
 app.listen(port, () => {
